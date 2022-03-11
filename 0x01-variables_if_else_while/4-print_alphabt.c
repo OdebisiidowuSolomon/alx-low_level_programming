599bae778453e1937c82947a641fbc1ecf0b13d0
Solomon <odebisiidowu09@gmail.com>
2022-03-12 00:21:50 +0100

    fixed betty issue

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
 /**
-* main - prints the alphabet in lowercase, and then in uppercase
-*
-* Return: Always 0 (Success) otherwise any other nummber
-*/
+ * main - prints out the alphabet in lowercase except for 'qe'
+ *
+ * Return (0)
+ */
 
 int main(void)
 {
-	char ch = 'a';
-	char Ch = 'A';
+	char c;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	for (Ch = 'A'; Ch <= 'Z'; Ch++)
-	{
-		putchar(Ch);
-	}
+	for (c = 'a'; c <= 'z'; c++)
+
+	if (c != 'e' && c != 'q')
+		putchar(c);
+
 	putchar('\n');
 
 	return (0);
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 /**
-* main - prints all possible combinations of single-digit numbers
-*
-* Return: Always 0 (Success) otherwise any other nummber
-*/
+ * main - Print single digit numbers in the formart
+ *
+ * Description: Sepaeated by commas
+ * Return: Always(0) Success
+ */
 
 int main(void)
 {
 	int i;
 
-    for (i = 48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-	    if (i != 57)
+	}
+	if (i != 57)
 	{
 		putchar(',');
 		putchar(' ');
 	}
-	}
 	putchar('\n');
-
 
 	return (0);
 }
