d010c2c40dfa82ac82959f36994d724b758b40d1
Solomon <odebisiidowu09@gmail.com>
2022-03-12 00:39:06 +0100

    fixed betty error

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the lowercase alphabet in reverse, followed by a new line
- *   *
- *    * You can only use the putchar function (every other function
- *     * (printf, puts, etc…) is forbidden)
- *      * All your code should be in the main function
- *       * You can only use putchar twice in your code
- *        * Return: 0
- */
+* main - prints the lowercase alphabet in reverse
+*
+* Return: Always 0 (Success) otherwise any other nummber
+*/
 
 int main(void)
 {
-int ch;
-for (ch = 122; ch > 96; ch--)
-{
-putchar(ch);
-}
-putchar('\n');
-return (0);
+	char ch = 'z';
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
 /**
-* main - prints all the numbers of base 16 in lowercase
-*
-* Return: Always 0 (Success) otherwise any other nummber
-*/
+ *  * main - prints the lowercase alphabet in reverse, followed by a new line
+ *   *
+ *    * You can only use the putchar function (every other function
+ *     * (printf, puts, etc…) is forbidden)
+ *      * All your code should be in the main function
+ *       * You can only use putchar twice in your code
+ *        * Return: 0
+ */
 
 int main(void)
 {
-	int i;
-
-    for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	for (i = 'a'; i <= 'f'; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-
-	return (0);
+int ch;
+for (ch = 122; ch > 96; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
