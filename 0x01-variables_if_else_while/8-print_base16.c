bdac0c8e18a24cdc5471de321576d064aad34153
Solomon <odebisiidowu09@gmail.com>
2022-03-12 00:40:50 +0100

    fixed errors

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
- *  * main - prints the lowercase alphabet in reverse, followed by a new line
- *   *
- *    * You can only use the putchar function (every other function
- *     * (printf, puts, etc…) is forbidden)
- *      * All your code should be in the main function
- *       * You can only use putchar twice in your code
- *        * Return: 0
+ ** main - Entry point
+ **
+ ** Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int ch;
-for (ch = 122; ch > 96; ch--)
-{
-putchar(ch);
-}
+int d;
+char low;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+for (low = 'a'; low <= 'f'; low++)
+putchar(low);
 putchar('\n');
 return (0);
 }
